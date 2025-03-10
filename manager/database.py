4bd1491d684d9dfb6cac3aaaa15ac2eaf3e08341
AkMlBBZo <iliyanik07@gmail.com>
2025-03-10 20:17:07 +0300

    I'm sorry, I'm so stupid

--- manager/database.py
+++ manager/database.py
@@ -87,6 +87,77 @@ class Database:
             else:
                 print(f"Text with ID {text_id} not found.")
 
+    # Users methods
+    def user_exists(self, user_id: int) -> bool:
+        """
+        check if user exists
+        """
+        with self.Session as session:
+            user = session.query(Users).filter(Users.id == user_id).first()
+            return not user is None
+
+    def add_user(self, user_id: int, username: str):
+        """
+        add new user
+        """
+        with self.Session as session:
+            user = Users(id=user_id, username=username)
+            session.add(user)
+    
+    def get_username(self, user_id: int) -> str:
+        """
+        get username of user by id
+        """
+        with self.Session as session:
+            user = session.query(Users).filter(Users.id == user_id).first()
+            return user.username if not user is None else None    
+
+    def get_role(self, user_id: int) -> int:
+        """
+        get role of user by id
+        """	
+        with self.Session as session:
+            user = session.query(Users).filter(Users.id == user_id).first()
+            return user.role if not user is None else -1
+    
+    def set_role(self, user_id: int, new_role: int) -> bool:
+        """
+        set role of user by id
+        """
+        with self.Session as session:
+            user = session.query(Users).filter(Users.id == user_id).first()
+            if not user is None:
+                user.role = new_role
+                session.commit()
+                return True
+            return False
+
+    # Basket methods
+    def add_basket_position(user_id: int, product_id: int, quantity: int):
+        """
+        add/change position in basket
+        """
+        with self.Session as session:
+            basket_position = session.query(Basket).filter(Basket.user_id == user_id, Basket.product_id == product_id).first()
+            if basket_position is None:
+                basket_position = Basket(user_id=user_id, product_id=product_id, quantity=quantity)
+                session.add(basket_position)
+            else:
+                basket_position.quantity = quantity
+                session.commit()
+        
+    def get_all_basket_positions(user_id: int) -> list:
+        """
+        get all positions in basket
+        returnable type: list<list<int>>
+        returnable name of types: [[product_id, quantity]]
+        """
+        with self.Session as session:
+            user_basket_positions = session.query(Basket).filter(Basket.user_id == user_id).all()
+            user_basket_positions = [[basket_position.product_id, basket_position.quantity] for basket_position in user_basket_positions]
+            return user_basket_positions if not user_basket_positions is None else []
+
+
 
 
 
